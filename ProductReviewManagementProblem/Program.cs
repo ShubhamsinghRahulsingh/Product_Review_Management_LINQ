186175dc2d42f72271da8e4e6d70ac3365a5d7f2
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-26 00:51:27 +0530

    (Shubham)Add: Project Folder Structure Created

---
+++ ProductReviewManagementProblem/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace ProductReviewManagementProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Product Review Management Problem ");
+        }
+    }
+}
