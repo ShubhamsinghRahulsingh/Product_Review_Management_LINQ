4c8ee38e3d2355da825e31194a3fe3ea480086e0
ShubhamsinghRahulsingh <shubh234singh@gmail.com>
2022-12-26 11:08:24 +0530

    (Shubham)Add: Adding Default Values to the Product Review List That we created

---
+++ ProductReviewManagementProblem/Management.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewManagementProblem
+{
+    public class Management
+    {
+        List<ProductReview> productReviews = new List<ProductReview>();
+        public void AddProductReviews(List<ProductReview> list)
+        {
+            this.productReviews = list;
+            Console.WriteLine("Reviews Added Successfully to the list");
+        }
+        public void PrintList(List<ProductReview> list)
+        {
+            foreach (ProductReview productReview in list)
+            {
+                Console.WriteLine("ProductID:" + productReview.ProductId + "  UserID:" + productReview.UserId + "  Rating:" + productReview.Rating + "  Review:" + productReview.Review + "     ISLIKE:" + productReview.IsLike);
+            }
+        }
+    }
+}
--- ProductReviewManagementProblem/Program.cs
+++ ProductReviewManagementProblem/Program.cs
@@ -6,6 +6,49 @@ namespace ProductReviewManagementProblem
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Product Review Management Problem ");
+            List<ProductReview> productReviews = new List<ProductReview>()
+            {
+                new ProductReview() { ProductId=1,UserId=1,Rating=4,Review="Good",IsLike=true},
+                new ProductReview() { ProductId=4,UserId=2,Rating=4,Review="Average",IsLike=false},
+                new ProductReview() { ProductId=11,UserId=3,Rating=2,Review="Good",IsLike=true},
+                new ProductReview() { ProductId=1,UserId=4,Rating=1,Review="Very Good",IsLike=true},
+                new ProductReview() { ProductId=2,UserId=5,Rating=3,Review="Bad",IsLike=false},
+                new ProductReview() { ProductId=3,UserId=6,Rating=5,Review="Good",IsLike=true},
+                new ProductReview() { ProductId=6,UserId=7,Rating=2,Review="Average",IsLike=true},
+                new ProductReview() { ProductId=1,UserId=8,Rating=4,Review="Very Good",IsLike=true},
+                new ProductReview() { ProductId=6,UserId=9,Rating=3,Review="Good",IsLike=true},
+                new ProductReview() { ProductId=5,UserId=10,Rating=5,Review="Average",IsLike=true},
+                new ProductReview() { ProductId=14,UserId=11,Rating=1,Review="Bad",IsLike=false},
+                new ProductReview() { ProductId=7,UserId=12,Rating=4,Review="Average",IsLike=true},
+                new ProductReview() { ProductId=9,UserId=13,Rating=5,Review="Good",IsLike=true},
+                new ProductReview() { ProductId=9,UserId=14,Rating=4,Review="Average",IsLike=true},
+                new ProductReview() { ProductId=7,UserId=15,Rating=3,Review="Very Good",IsLike=true},
+                new ProductReview() { ProductId=2,UserId=16,Rating=4,Review="Average",IsLike=true},
+                new ProductReview() { ProductId=1,UserId=17,Rating=2,Review="Average",IsLike=true},
+                new ProductReview() { ProductId=3,UserId=18,Rating=4,Review="Bad",IsLike=false},
+                new ProductReview() { ProductId=3,UserId=19,Rating=3,Review="Good",IsLike=true},
+                new ProductReview() { ProductId=1,UserId=20,Rating=3,Review="Average",IsLike=true}
+            };
+            Management manage = new Management();
+            bool flag = true;
+            while(flag)
+            {
+                Console.WriteLine("--------------------------------------");
+                Console.WriteLine("Select From Given Operations\n\n1.Add Default values to the list we created\n2.Display List\n3.Exit");
+                int sel=Convert.ToInt32(Console.ReadLine());
+                switch(sel)
+                {
+                    case 1:
+                        manage.AddProductReviews(productReviews);
+                        break;
+                    case 2:
+                        manage.PrintList(productReviews);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                }
+            }
         }
     }
 }
